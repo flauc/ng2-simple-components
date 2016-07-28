df06dc77422164fa3fc2a1290f0a1f778bffd793
flauc <filip.lauc93@gmail.com>
2016-07-28 23:08:55 +0200

    test: Testing ViewContainerRef

--- example/app/app.component.ts
+++ example/app/app.component.ts
@@ -1,9 +1,13 @@
 import {Component} from '@angular/core';
+import {RippleComponent} from './ripple.component';
+import {RippleDirective} from './ripple.directive';
 
 @Component({
     selector: 'app',
+    directives: [RippleComponent, RippleDirective],
     template: `
         <p>Working</p>
+        <p sc-ripple>Proba</p>
     `
 })
 export class AppComponent { }
