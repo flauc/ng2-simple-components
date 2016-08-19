7d2fc535283b4bca18274e199a5dab98d41c6e27
flauc <filip.lauc93@gmail.com>
2016-08-19 19:55:40 +0200

    chore: Animation update

--- example/app/on-position/animation.directive.ts
+++ example/app/on-position/animation.directive.ts
@@ -1,18 +1,27 @@
 import {Directive, HostListener, ElementRef, Input, Output, EventEmitter} from '@angular/core';
 
 @Directive({
-    selector: '[scAnimation]'
+    selector: '[sc-animation]'
 })
 export class AnimationDirective {
 
-    @Input() refEl: any;
-
+    // Element that triggers the reached state
+    @Input('sc-animation') refEl: any;
+    // Name of the reached state
     @Input() animation: string = 'reached';
+    // Name of the state parameter
     @Input() state: string = 'animationState';
+    // An offset from the top of the refEl
     @Input() offset: number = 0;
+
     @Output() reached: EventEmitter<any> = new EventEmitter(null);
 
-    @HostListener('window:scroll', ['$event']) onScroll(event: Event) {
-        if (this.refEl.getBoundingClientRect().top + this.offset <= 0) this.refEl[this.state] = this.animation;
+    @HostListener('window:scroll', ['$event']) onScroll() {
+        if (this.refEl.getBoundingClientRect().top + parseFloat(this.offset) <= 0) {
+            this._el.nativeElement[this.state] = this.animation;
+            this.reached.emit(true)
+        }
     }
+
+    constructor(private _el: ElementRef) {}
 }
