6c9ee65c00518f9c20b10729b4960bb86a70ce1c
flauc <filip.lauc93@gmail.com>
2016-08-27 23:03:48 +0200

    fix: missing encapsulation on accordion

--- src/accordion/accordion.component.ts
+++ src/accordion/accordion.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, ViewEncapsulation} from '@angular/core';
 import {AccordComponent} from './accord.component';
 
 @Component({
