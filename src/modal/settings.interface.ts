816d3f73969edd0c87f7c10f6082b5f58822f05d
Filip Lauc <filip.lauc93@gmail.com>
2016-10-03 18:22:53 +0200

    fix: Modal settings need to be optional

--- src/modal/settings.interface.ts
+++ src/modal/settings.interface.ts
@@ -1,6 +1,6 @@
 export interface ModalSettings {
-    overlay: boolean
-    overlayClickToClose: boolean
-    defaultFooter: boolean
-    showCloseButton: boolean
+    overlay?: boolean
+    overlayClickToClose?: boolean
+    defaultFooter?: boolean
+    showCloseButton?: boolean
 }
